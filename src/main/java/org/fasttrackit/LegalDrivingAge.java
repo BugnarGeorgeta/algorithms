afbd36db738f7a9fa8fcf600638ac0d6788d4d92
GetaBugnar <bugnargeta@gmail.com>
2021-04-16 12:45:38 +0300

    added Ternary operator and IllegalArgumentException()

--- src/main/java/org/fasttrackit/LegalDrivingAge.java
+++ src/main/java/org/fasttrackit/LegalDrivingAge.java
@@ -4,6 +4,10 @@ package org.fasttrackit;
 //        older, then the program should display “You are old enough
 //        to legally drive.” If the user is under sixteen, the program
 //        should display “You are not old enough to legally drive.”
+//        Use a ternary operator to write this program.
+//If the user enters a number that’s less than zero or enters
+//        non-numeric data, display an error message that asks
+//        the user to enter a valid age.
 
 
 import java.util.InputMismatchException;
@@ -11,30 +15,28 @@ import java.util.Scanner;
 
 public class LegalDrivingAge {
 
-    public int compareAge() {
-        System.out.println("What is your age?");
-        Scanner scanner = new Scanner(System.in);
-        int age;
+    private void compareAge() {
         try {
+            System.out.println("What is your age?");
+            Scanner scanner = new Scanner(System.in);
+            int age;
+
             age = scanner.nextInt();
-            if (age >= 18 && age < 80) {
-                System.out.println("You are old enough to legally drive");
-            } else if (age < 0) {
-                System.out.println("Please enter correctly your age.");
-            } else if (age >= 80) {
-                System.out.println("Sorry, You are too old to drive.");
-            } else {
-                System.out.println("You are not old enough to legally drive.");
+            if (age < 0 || age >80) {
+                throw new IllegalArgumentException();
             }
 
-        } catch (InputMismatchException e) {
-            System.out.println("Please enter the data correctly");
-            return compareAge();
+            String msg = (age >= 18) ? "You are old enough to legally drive"
+                    : "You are not old enough to legally drive.";
+            System.out.println(msg);
+
+        } catch (InputMismatchException | IllegalArgumentException e) {
+            System.out.println("Please enter a valid age, non-negative and below 80");
+            compareAge();
 
         }
-        return age;
-    }
 
+    }
 
     public static void main(String[] args) {
         LegalDrivingAge legalDrivingAge = new LegalDrivingAge();
